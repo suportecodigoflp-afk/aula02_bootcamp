365948edf6900a3cbb0d8a5ca746a0d29b03d99d
AlexBueno <suportecodigoflp@gmail.com>
2026-02-12 19:20:13 -0300

    adicionado exercicios 04

---
+++ exercicios.py
@@ -0,0 +1,12 @@
+## Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+## Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+##Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+
+numero_01 = int(input("inserir um numero inteiro:"))
+numero_02 = int(input("inserir outro numero inteiro:"))
+
+resultado = numero_01 // numero_02
+
+print(resultado)
+
